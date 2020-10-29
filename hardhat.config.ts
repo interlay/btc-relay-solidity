be74af010f5041f40a66fc4e0ef28cf93aa0e81e
ns212 <>
2020-10-29 12:04:52 +0000

    migrate to hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 require('@nomiclabs/hardhat-ganache');
 require('@nomiclabs/hardhat-waffle');
 require('hardhat-typechain');
-import "@nomiclabs/hardhat-ethers";
+import '@nomiclabs/hardhat-ethers';
 // require('buidler-gas-reporter');
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
-const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY || '5de75329e619948d55744d85d763790ae3f7643f0a498070558acdb37d6b2057'; // Dummy wallet
+const ROPSTEN_PRIVATE_KEY =
+  process.env.ROPSTEN_PRIVATE_KEY ||
+  '5de75329e619948d55744d85d763790ae3f7643f0a498070558acdb37d6b2057'; // Dummy wallet
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
