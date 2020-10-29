9eb83ddce5ebf28f79795120b941ea25ae8e0abd
ns212 <>
2020-10-29 08:22:24 +0000

    migrate to hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ require('hardhat-typechain');
 // require('buidler-gas-reporter');
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
-const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY || '';
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY || '5de75329e619948d55744d85d763790ae3f7643f0a498070558acdb37d6b2057'; // Dummy wallet
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
